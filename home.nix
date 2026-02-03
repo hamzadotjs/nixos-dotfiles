c88c3466079b227b407770df0149b26b07217db3
hamza.nix <12.hamza.sherif@gmail.com>
2026-02-03 16:45:16 +0200

    symlink configuration.nix and home.nix

---
+++ home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "mark";
+  home.homeDirectory = "/home/mark";
+  home.stateVersion = "25.11";
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nixoconfig = "sudo hx /etc/nixos/configuration.nix";
+      homeconfig = "sudo hx /etc/nixos/home.nix";
+      nrs = "sudo nixos-rebuild switch";
+    };
+
+    initExtra = ''
+      export PS1="\u@\h \[\033[38;2;67;163;40m\]\w\[\033[0m\] $ "
+      neofetch
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.opacity = 0.9;
+      font.normal = {
+        family = "JetBrains Mono Nerd Font";
+        style = "Regular";
+      };
+      font.size = 14;
+    };
+  };
+
+  home.packages = with pkgs; [
+    bat
+  ];
+}
+
